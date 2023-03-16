43c41f46d7292c73e6305c85683ae3550e11779c
Nelson Duarte <nelsonduarte@gmail.com>
2023-03-16 16:01:02 +0000

    send event to analytics engine

--- src/types.ts
+++ src/types.ts
@@ -38,3 +38,52 @@ export type Email = {
   text?: string
   attachments: Attachment[]
 }
+
+export type DmarcRecordRow = {
+  reportMetadataReportId: string
+  reportMetadataOrgName: string
+  reportMetadataDateRangeBegin: number
+  reportMetadataDateRangeEnd: number
+  reportMetadataError: string
+
+  policyPublishedDomain: string
+  policyPublishedADKIM: AlignmentType
+  policyPublishedASPF: AlignmentType
+  policyPublishedP: DispositionType
+  policyPublishedSP: DispositionType
+  policyPublishedPct: number
+
+  recordRowSourceIP: string
+  recordRowCount: number
+  recordRowPolicyEvaluatedDKIM: DMARCResultType
+  recordRowPolicyEvaluatedSPF: DMARCResultType
+  recordRowPolicyEvaluatedDisposition: DispositionType
+  recordRowPolicyEvaluatedReasonType: PolicyOverrideType
+  recordIdentifiersEnvelopeTo: string
+  recordIdentifiersHeaderFrom: string
+}
+
+export enum AlignmentType { // eslint-disable-line no-shadow
+  r = 0, // eslint-disable-line id-length
+  s = 1, // eslint-disable-line id-length
+}
+
+export enum DMARCResultType { // eslint-disable-line no-shadow
+  fail = 0,
+  pass = 1,
+}
+
+export enum DispositionType { // eslint-disable-line no-shadow
+  none = 0,
+  quarantine = 1,
+  reject = 2,
+}
+
+export enum PolicyOverrideType { // eslint-disable-line no-shadow
+  other = 0,
+  forwarded = 1,
+  sampled_out = 2,
+  trusted_forwarder = 3,
+  mailing_list = 4,
+  local_policy = 5,
+}
